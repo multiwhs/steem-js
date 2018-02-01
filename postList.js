3c5abc4ae92074284425e928afdfd381d090be9d
multiwhs <jhjung@idis.co.kr>
2018-02-01 09:43:10 +0000

    Add files via upload

---
+++ postList.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const steem = require('steem')
+steem.api.setOptions({ url: 'https://api.steemit.com' });
+
+var authors = ['tradingideas', 'musicholic']
+var beforeDate = '2018-01-30T09:00:00'
+var str
+
+beforeDate = new Date().toISOString()
+console.log(beforeDate)
+beforeDate = beforeDate.split('.')[0];
+
+for (var k = 0; k < authors.length; k++) {
+    steem.api.getDiscussionsByAuthorBeforeDate(authors[k], '', beforeDate, 10, function (err, post) {
+        if (err === null) {
+            var str = "author : " + post[0].author + '\n =================== \n'
+            console.log(str)
+            for (var i = 0; i < post.length; i++) {
+                str = post[i].created + ' : ' + post[i].title + ' : ' + post[i].permlink + ' : ' + post[i].pending_payout_value.split(' ')[0]
+                console.log(str)
+            }
+            console.log('\n')
+        }
+    });
+}
+
