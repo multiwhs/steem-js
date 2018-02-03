b761ab2ee4fdb9ac10f6131ee2e5bcd5cb8312cd
multiwhs <jhjung@idis.co.kr>
2018-02-03 16:33:24 +0000

    Add files via upload
    voting example :  example usage of  the following function;
    steem.broadcast.vote(wif, voter, author, permlink, weight, function (err, voted)

---
+++ auto_voting_simple.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const steem = require('steem')
+steem.api.setOptions({ url: 'https://api.steemit.com' });
+
+var voter = 'tradingideas'
+var wif = '5xxxx'   // posting key
+var author = 'tradingideas'
+var permlink = '5nnwke-test'
+var weight = 100    // 1.0%
+
+steem.broadcast.vote(wif, voter, author, permlink, weight, function (err, voted) {
+    if (err === null) {
+        console.log("voting success");
+        steem.api.getActiveVotes(author, permlink, function (err, response) {
+            for (var i = 0; i < response.length; i++) {
+                if (response[i].voter == voter) {
+                    var msg = "voted ok" + response[i].rshares
+                    console.log(msg);
+                }
+            }
+        });
+    }
+    else {
+        console.log(err.message.split("{}")[0])
+    }
+});
+
